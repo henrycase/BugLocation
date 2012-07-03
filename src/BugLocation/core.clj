380b6423ad05834438130df9d38c32f4f8923b3d
Henry Case <henry@metacircular.net>
2012-07-02 21:55:45 -0600

    cleaning up the codebase

--- src/BugLocation/core.clj
+++ src/BugLocation/core.clj
@@ -1,18 +1,37 @@
 (ns BugLocation.core
-  (:gen-class))
+  (:gen-class)
+  (:use [clojure.xml :only (parse)]))
 
 (def bug-dev "192.168.2.63")
-(defonce bug-host bug-dev)           ; normally 127.0.0.1
+(def bug-live "127.0.0.1")
+(defonce bug-host bug-live)           ; normally 127.0.0.1
+
+(defn hasLocationService?
+  []
+  (let [services-xml (:content (parse (format "http://%s/service"
+                                              bug-host)))]
+    (< 0 (count
+          (filter #(= (:name %) "Location")
+                  (map :attrs
+                       (filter #(= (:tag %) :service) services-xml)))))))
 
 (defn getLocation
   "Poll the bug's location service."
   []
-  (let [location (slurp (format "http://%s/service/Location" bug-host))]
-    location))
+  (let [location-xml (parse (format "http://%s/service/Location" bug-host))]
+    (doall (:content location-xml))))
 
 
 (defn -main [& args]
-  (println "[+] polling...")
-  (while true
-    (Thread/sleep 1000)
-    (println (getLocation))))
+  (println "[+] checking for location service...")
+  (if (hasLocationService?)
+    (do
+      (println "[+] location service found.")
+      (println "[+] polling...")
+      (while true
+        (Thread/sleep 1000)
+        (let [location (getLocation)]
+          (if-not (nil? location)
+            (println location)
+            (println "[!] no fix!")))))
+    (println "[!] location service not found!")))
